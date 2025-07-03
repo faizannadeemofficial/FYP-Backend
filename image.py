64926a227d3fbe867427b637d2cfaf16a1ab9409
faizannadeemofficial <imfaizannadeem@gmail.com>
2025-07-03 16:47:34 +0500

    Code updated

--- image.py
+++ image.py
@@ -36,7 +36,7 @@ class ImageProfanityFilter:
         blurred_image = image.filter(ImageFilter.GaussianBlur(radius=blur_radius))
 
         # Save the output
-        output_path = f"storage/imgs/{int(time.time())}.jpg"
+        output_path = f"storage/files/{int(time.time())}.jpg"
         blurred_image.save(output_path)
 
         return output_path
